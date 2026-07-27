0f4bbab69f342675cfd7450322f52390512ae2c9
Evgeniy Gurskiy <gurskiy27@mail.ru>
2026-07-28 00:07:04 +0600

    Исправил замечания

--- src/ru/yandex/practicum/delivery/ParcelBox.java
+++ src/ru/yandex/practicum/delivery/ParcelBox.java
@@ -6,42 +6,30 @@ import java.util.List;
 public class ParcelBox<T extends Parcel> {
 
     private int maxWeight;
-
+    private int currentWeight;
     private List<T> parcels = new ArrayList<>();
-
 
     public ParcelBox(int maxWeight) {
         this.maxWeight = maxWeight;
+        this.currentWeight = 0;
     }
-
 
     public void addParcel(T parcel) {
 
-        int currentWeight = 0;
-
-        for (T item : parcels) {
-            currentWeight += item.getWeight();
-        }
-
-
         if (currentWeight + parcel.getWeight() > maxWeight) {
-
             System.out.println(
                     "Нельзя добавить посылку <<"
                             + parcel.getDescription()
                             + ">>. Превышен максимальный вес коробки."
             );
-
             return;
         }
 
-
         parcels.add(parcel);
+        currentWeight += parcel.getWeight();
     }
 
-
     public List<T> getAllParcels() {
-
         return new ArrayList<>(parcels);
     }
 }
